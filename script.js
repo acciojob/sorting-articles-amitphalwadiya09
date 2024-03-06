24ed0990cdbae00cc3046a0611f7896be920067e
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-06 18:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 function remove(str) {
 	str=str+" "
-	let newstr=str.replaceAll("the ","").trim();
-	 newstr=str.replaceAll("The ","").trim();
-	 newstr=str.replaceAll("a ","").trim();
-	 newstr=str.replaceAll("A ","").trim();
-	 newstr=str.replaceAll("an ","").trim();
-	 newstr=str.replaceAll("An ","").trim();
+	let newstr=str.replace("the ","").trim();
+	 newstr=str.replace("The ","").trim();
+	 newstr=str.replace("a ","").trim();
+	 newstr=str.replace("A ","").trim();
+	 newstr=str.replace("an ","").trim();
+	 newstr=str.replace("An ","").trim();
 	return newstr
 }
 
