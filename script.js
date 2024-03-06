509a5414be2e0b453edfdea37e7a7ed7604d9ceb
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-06 19:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-document.addEventListener('DOMContentLoaded', function () {
-  // Your JavaScript code here
-	function remove(str) {
+
+function remove(str) {
   // Use a regular expression to replace specified prefixes
   return str.replace(/^(the|a|an)\s+/i, '').trim();
 }
@@ -17,6 +16,3 @@ bands.forEach(band => {
   listItem.textContent = band;
   bandList.appendChild(listItem);
 });
-
-});
-
